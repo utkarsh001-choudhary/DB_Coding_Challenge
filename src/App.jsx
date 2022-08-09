3a44c4cbd592bdcde1fa315599026b2eb35807ec
Shamik21 <shamikmitra21@gmail.com>
2022-08-09 19:58:14 +0530

    login page update 1

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 // import { Router, Route, Switch, Redirect } from "react-router-dom";
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 // import { Pets } from "./components/pets/Pets";
@@ -9,12 +9,40 @@ import Login from "./components/Auth/Login";
 import 'bootstrap/dist/css/bootstrap.min.css';
 // import { Routes} from 'react-router-dom';
 const App = () => {
+
+  const adminUser = {
+    email : "admin@db.com",
+    password: "admin123"
+  }
+
+  const [user, setUser] = useState({name:"", email:" "});
+  const [error, setError]= useState("");
+
+  const handleLogin = details => {
+    console.log(details);
+
+    if (details.email == adminUser.email && details.password == adminUser.password) {
+      console.log("Logged in");
+      setUser({
+        name: details.name,
+        email: details.email
+      });
+    } else {
+      console.log("Details do not match!");
+      setError("Details do not match!");
+    }
+  }
+
+  const Logout = details => {
+    console.log("Logout");
+  }
+
   return (
   // return <Pets/>
   <Router history={history}>
          <Routes>
            <Route exact path="/" element={<Books />} />
-           <Route exact path="/login" element={<Login/>} />
+           <Route exact path="/login" element={<Login Login={handleLogin} error={error}/>} />
             {/* specific details page */}
             <Route exact path="/book" element={<Book/>} />
          </Routes>
--- src/components/Auth/Login.jsx
+++ src/components/Auth/Login.jsx
@@ -1,43 +1,35 @@
-import React, { useState, useEffect } from "react";
-import {Form, Button} from 'react-bootstrap';
+import React, { useState } from 'react'
+import styles from "./Login.module.css";
 
-const Login = () => {
+function Login({Login,error}) {
+    const [details, setDetails] = useState({name:"", email:"",password:""});
+    
+    const submitHandler = e => {
+        e.preventDefault();
 
-	const [user, setUser] = useState("");
-	const [pass, setPass] = useState("");
-
-	const handleUserChange = (e) => {
-		setUser(e.target.value);
-	}
-
-	const handlePassChange = (e) => {
-		setPass(e.target.value);
-	}
-
-	const verifyUserPass = () => {
-		console.log("User: ", user);
-		console.log("Pass: ", pass);
-	}
-
-	return (
-		<div style={{ height: '100vh', alignItems: 'center', display: 'flex', flexDirection: 'row', justifyContent: 'space-around' }}>
-			<h1 style={{fontSize: '4rem'}}>The Bonds App</h1>
-			
-			<Form style={{boxShadow: '0px 0px 7px rgba(0,0,0,0.4)', padding: '30px', borderRadius: '10px'}}>
-				<Form.Group style={{marginBottom: '1rem'}} >
-					<Form.Label style={{fontSize: '1.5rem'}}>Email ID</Form.Label><br/>
-					<Form.Control style={{width: 'auto', fontSize: '1.5rem'}} type="email" placeholder="Enter your email" value={user} onChange={handleUserChange} />
-				</Form.Group>
-				<Form.Group style={{marginBottom: '1rem'}} >
-					<Form.Label style={{fontSize: '1.5rem'}}>Enter your password</Form.Label><br/>
-					<Form.Control style={{width: 'auto', fontSize: '1.5rem'}} type="password" placeholder="Enter your password" value={pass} onChange={handlePassChange} />
-				</Form.Group>
-				<Button type="submit" onClick={verifyUserPass}>Submit</Button>
-			</Form>
-			
-		</div>
-  	)
-};
-
+        Login(details);
+    }
+    return(
+        <form onSubmit={submitHandler}>
+            <div className="form-inner">
+                <h2>Login</h2>
+                {(error != "") ? ( <div className="error">{error}</div> ) : "" }
+                <div className="form-group">
+                    <label htmlFor="name">Name:</label>
+                    <input type='text' name='name' id='name' onChange={e => setDetails({...details,name: e.target.value})} value={details.name} />
+                </div>
+                <div className="form-group">
+                    <label htmlFor='email'>Email:</label>
+                    <input type='email' name="email" id='email' onChange={e => setDetails({...details,email: e.target.value})} value={details.email}/>
+                </div>
+                <div className="form-group">
+                    <label htmlFor='password'>Password:</label>
+                    <input type='password' name="password" id='password' onChange={e => setDetails({...details,password: e.target.value})} value={details.password}/>
+                </div>
+                <input type="submit" value="LOGIN" />
+            </div>
+        </form>
+    )
+}
 
 export default Login;

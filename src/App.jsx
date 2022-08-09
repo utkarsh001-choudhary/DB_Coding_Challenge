bf02b2622c788015844cb4a6ae4086aac00251b0
Shamik21 <shamikmitra21@gmail.com>
2022-08-09 12:14:22 +0530

    Added login screen

--- src/App.jsx
+++ src/App.jsx
@@ -4,13 +4,16 @@ import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import { Pets } from "./components/pets/Pets";
 import { history } from "./History";
 import Dummy from "./components/Dummy/dummy";
+import Login from "./components/Auth/Login";
+import 'bootstrap/dist/css/bootstrap.min.css';
 // import { Routes} from 'react-router-dom';
 const App = () => {
   return (
   // return <Pets/>
   <Router history={history}>
          <Routes>
-            <Route exact path="/" element={<Pets/>} />
+           <Route exact path="/" element={<Pets />} />
+           <Route exact path="/login" element={<Login/>} />
             {/* specific details page */}
             <Route exact path="/show" element={<Dummy/>} />
          </Routes>
---
+++ src/components/Auth/Login.jsx
@@ -0,0 +1,43 @@
+import React, { useState, useEffect } from "react";
+import {Form, Button} from 'react-bootstrap';
+
+const Login = () => {
+
+	const [user, setUser] = useState("");
+	const [pass, setPass] = useState("");
+
+	const handleUserChange = (e) => {
+		setUser(e.target.value);
+	}
+
+	const handlePassChange = (e) => {
+		setPass(e.target.value);
+	}
+
+	const verifyUserPass = () => {
+		console.log("User: ", user);
+		console.log("Pass: ", pass);
+	}
+
+	return (
+		<div style={{ height: '100vh', alignItems: 'center', display: 'flex', flexDirection: 'row', justifyContent: 'space-around' }}>
+			<h1 style={{fontSize: '4rem'}}>The Bonds App</h1>
+			
+			<Form style={{boxShadow: '0px 0px 7px rgba(0,0,0,0.4)', padding: '30px', borderRadius: '10px'}}>
+				<Form.Group style={{marginBottom: '1rem'}} >
+					<Form.Label style={{fontSize: '1.5rem'}}>Email ID</Form.Label><br/>
+					<Form.Control style={{width: 'auto', fontSize: '1.5rem'}} type="email" placeholder="Enter your email" value={user} onChange={handleUserChange} />
+				</Form.Group>
+				<Form.Group style={{marginBottom: '1rem'}} >
+					<Form.Label style={{fontSize: '1.5rem'}}>Enter your password</Form.Label><br/>
+					<Form.Control style={{width: 'auto', fontSize: '1.5rem'}} type="password" placeholder="Enter your password" value={pass} onChange={handlePassChange} />
+				</Form.Group>
+				<Button type="submit" onClick={verifyUserPass}>Submit</Button>
+			</Form>
+			
+		</div>
+  	)
+};
+
+
+export default Login;
